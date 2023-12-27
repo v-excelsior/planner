9df6cb033e24b6e96609867077f4406019037a37
Dima <vakyla98@gmail.com>
2023-12-27 06:48:17 +0200

    serve static on with express

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ const express = require("express")
 const app = express()
 const cors = require('cors')
 const fs = require('fs')
+const path = require('path')
 
 const { readPlans } = require('./operations')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, '../build')))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve('../build/index.html'));
+});
 
 // get all plans
 app.get('/plans', async (req, res) => {
