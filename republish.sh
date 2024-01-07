7b318b280976df1c2ce82fac6aa26074afb70305
Dima <vakyla98@gmail.com>
2024-01-07 12:54:38 +0200

    auto build

--- republish.sh
+++ republish.sh
@@ -1,5 +1,28 @@
+echo "Removing old build..."
 git pull
+echo "Removing old build..."
 
-cd ./client
+echo "Removing old build..."
+rm -rf build
+echo "Removing old build DONE!"
 
+echo "Updating SERVER deps..."
+cd ./server || exit
+echo "Removing old SERVER deps..."
+rm -rf node_modules
+echo "Installing new SERVER deps..."
+npm i
+echo "Updating SERVER DONE!"
+
+echo "Updating CLIENT deps..."
+cd ../client || exit
+echo "Removing old CLIENT deps..."
+rm -rf node_modules
+echo "Installing new CLIENT deps..."
+npm i
+echo "Updating CLIENT DONE!"
+
+
+echo "Building CLIENT..."
 npm run build
+echo "Building CLIENT DONE!"
