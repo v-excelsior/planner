d1b21690824b1a70162bba3bd9f1f3d381e44e72
Dima <vakyla98@gmail.com>
2024-01-09 00:21:37 +0200

    fix api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+const LOCALHOST = 'http://localhost:3001'
+const PROD = 'http://v-excelsior.ddns.net'
+
 export const apiClient = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.NODE_ENV === 'development' ? LOCALHOST : PROD,
 })
