f677144d9f4116474b013dada04236dc657cad1d
Dima <vakyla98@gmail.com>
2024-01-14 18:05:14 +0200

    fix cors

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const LOCALHOST = 'http://localhost:3001'
-const PROD = 'http://v-excelsior.ddns.net'
+const PROD = 'http://192.168.0.102:3001'
 
 export const apiClient = axios.create({
   baseURL: process.env.NODE_ENV === 'development' ? LOCALHOST : PROD,
